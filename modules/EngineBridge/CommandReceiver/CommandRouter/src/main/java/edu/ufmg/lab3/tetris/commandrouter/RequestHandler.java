7c6943c04bd7e59da36c94b7d8526dee9bf53143
Victor de Lima Soares <vdls@uw.edu>
2016-09-29 20:29:50 -0300

    includes a logger for debugging

--- modules/EngineBridge/CommandReceiver/CommandRouter/src/main/java/edu/ufmg/lab3/tetris/commandrouter/RequestHandler.java
+++ modules/EngineBridge/CommandReceiver/CommandRouter/src/main/java/edu/ufmg/lab3/tetris/commandrouter/RequestHandler.java
@@ -1,16 +1,18 @@
 package edu.ufmg.lab3.tetris.commandrouter;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
 public class RequestHandler implements Runnable {
-	
+
+	private static Log log = LogFactoryUtil.getLog(RequestHandler.class);
 	private Socket client;
 
 	public RequestHandler(Socket client) {
@@ -21,13 +23,16 @@ public class RequestHandler implements Runnable {
 	public void run() {
 		try (DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
 				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));) {
+
 			Message msg = new Message();
-			
+
 			msg.setApp(in.readByte());
 			msg.setPlayer(in.readByte());
 			msg.setMove(in.readByte());
 			msg.setUnits(in.readByte());
-			
+
+			log.debug("msg: " + msg);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
