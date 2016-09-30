5733f0519a2530455dc69ab0118a76b63fe96ba1
Victor de Lima Soares <vdls@uw.edu>
2016-09-29 21:17:50 -0300

    enginecommander skeleton

---
+++ modules/EngineBridge/CommandReceiver/EngineCommander/src/main/java/edu/ufmg/lab3/tetris/enginebridge/commandReceiver/enginecommander/EngineCommander.java
@@ -0,0 +1,10 @@
+package edu.ufmg.lab3.tetris.enginebridge.commandReceiver.enginecommander;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class EngineCommander{
+
+
+
+}
